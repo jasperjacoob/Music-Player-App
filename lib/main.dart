5018963584e732d49117e10303341d28193000cb
GeraldOrzal <geraldorzal18@gmail.com>
2022-07-05 15:34:40 +0800

    editted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:musicplayer/routes.dart';
 import 'package:musicplayer/screens/admin/dashboard_screen.dart';
+import 'package:musicplayer/screens/auth/signin_screen.dart';
 import 'package:musicplayer/screens/client/musiccli_screen.dart';
 
 void main() {
@@ -14,11 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: generateRoute,
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: generateRoute,
+        home: SigninScreen());
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:musicplayer/screens/admin/dashboard_screen.dart';
+import 'package:musicplayer/screens/client/musiccli_screen.dart';
 
 import 'constant.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case dashboardRoute:
-      return MaterialPageRoute(builder: (_) => DashboardScreen());
+      return MaterialPageRoute(builder: (_) => MusiccliScreen());
     default:
       return MaterialPageRoute(
           builder: (_) => Scaffold(
